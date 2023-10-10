8633cb70935b0932bddf2d589ad449ff25903a24
TheBoatyMcBoatFace <bentleyhensel@gmail.com>
2023-10-10 15:02:07 -0400

    f
    Signed-off-by: TheBoatyMcBoatFace <bentleyhensel@gmail.com>

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 # app/__init__.py
-from .utils import configure_monitoring
+from .utils import configure_monitoring, logger
 from dotenv import load_dotenv
 
+from .database.postgres.connect import test_connection
 
 def startup():
+    logger.info('Starting up...')
     load_dotenv()
     configure_monitoring()
+    test_connection()
